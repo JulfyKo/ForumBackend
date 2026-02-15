cfd82a0e40f24c24463b012434cc9244fb11cd53
Віктор Павлюк <pavlyuvictor@gmail.com>
2026-02-15 13:26:23 +0200

    Add comment CRUD

--- Discussion/urls.py
+++ Discussion/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import CRUD_Discussion
+from .views import CRUD_Discussion, CRUD_Comment
 
 router = DefaultRouter()
 router.register("discussions", CRUD_Discussion)
+router.register("comments", CRUD_Comment )
 
 urlpatterns = router.urls
--- Discussion/views.py
+++ Discussion/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 
 from .models import Discussion, Comment
 from .serializators import DiscussionSerializer, CommentSerializer
@@ -7,3 +8,9 @@ from .serializators import DiscussionSerializer, CommentSerializer
 class CRUD_Discussion(ModelViewSet):
     queryset = Discussion.objects.all()
     serializer_class = DiscussionSerializer
+
+class CRUD_Comment(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+        
